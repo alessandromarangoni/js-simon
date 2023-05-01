38666873856f12f5ee79b474b792d1c795766a58
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-05-02 01:42:16 +0200

    sostituito alert con innerhtml

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
 let numeriHtml = document.getElementById("numeri");
-let bottone = document.getElementById("button")
+let bottone = document.getElementById("button");
+let risultatoHTML = document.getElementById("risultato");
 // devo inserire 5 numeri che non ho in un array
-let numeriBanco = []
+let numeriBanco = [];
 let numBCiclo;
 // questi numeri devono essere casuali
 for (let i = 0; i < 5; i++) {
-    numBCiclo = parseInt(Math.random() * 100)
-    console.log(numBCiclo)
-    pushoNumeri(numeriBanco,numBCiclo)
+    numBCiclo = parseInt(Math.random() * 100);
+    console.log(numBCiclo);
+    pushoNumeri(numeriBanco,numBCiclo);
 }
 // vanno poi mostrati a schermo
-numeriHtml.innerHTML=numeriBanco
+numeriHtml.innerHTML=numeriBanco;
 
 // questi numeri devono sparire dopo 30 secondi
 function rimuovinumeri(){
-    numeriHtml.style.display="none"
+    numeriHtml.style.display="none";
 }
 const timerNumeri = setInterval(rimuovinumeri, 30 * 1000);
 
 // devo ricevere 5 numeri dell utente
-let numeriUtente = []
+let numeriUtente = [];
 let numUCiclo;
 // imposto il timer di 3 sec per ora
-setTimeout(cicloNumeri, 30.1 * 1000)
+setTimeout(cicloNumeri, 30.1 * 1000);
 
 function cicloNumeri(){
     for (let i = 0; i < 5; i++) {
-        numUCiclo = parseInt(prompt("inserisci numeri"))
-        pushoNumeri(numeriUtente,numUCiclo)
+        numUCiclo = parseInt(prompt("inserisci numeri"));
+        pushoNumeri(numeriUtente,numUCiclo);
     }
 }
 // funzione che puscia i numeri in un array
 
 function pushoNumeri(arr,val){
-    arr.push(val)
+    arr.push(val);
 }
 
-console.log(numeriBanco)
+console.log(numeriBanco);
 
 function comparoNumeri (a, b){
     if (a.length !== b.length) return false;
@@ -52,16 +53,16 @@ function comparoNumeri (a, b){
 };
 
 bottone.addEventListener("click",function(){
-    let paragone = comparoNumeri(numeriBanco,numeriUtente)
+    let paragone = comparoNumeri(numeriBanco,numeriUtente);
     console.log(paragone);
 
     if( paragone == true){
-    alert(`hai vinto i numeri del banco sono ${numeriBanco} i tuoi ${numeriUtente}`)
+        risultatoHTML.innerHTML=`hai vinto!! i numeri del banco sono ${numeriBanco} i tuoi ${numeriUtente}`;
     clearInterval(timerNumeri)
 }
 else{
     clearInterval(timerNumeri)
-    alert(`hai perso i numeri del banco sono ${numeriBanco} i tuoi ${numeriUtente}`)
+    risultatoHTML.innerHTML=`hai perso!! i numeri del banco sono ${numeriBanco} i tuoi ${numeriUtente}`;
 }
-console.log(numeriUtente)
+console.log(numeriUtente);
 })
